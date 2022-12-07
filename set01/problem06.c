d783b691d719c958f9e8be592e1e38d575769b0e
Finni Sam  Sam <115693748+finnisam@users.noreply.github.com>
2022-12-07 08:35:28 +0000

    changes

---
+++ set01/problem06.c
@@ -0,0 +1,24 @@
+{
+  int largest;
+  if((a>b)&&(a>c))
+    largest=a;
+  if((b>a)&&(b>c))
+    largest=b;
+  if ((c>a)&&(c>b))
+    largest=c;
+  return largest;
+}
+void output (int a, int b, int c,int largest)
+{
+  printf("The largest of %d %d and %d is %d",a,b,c,largest);
+}
+int main()
+{
+  int a,b,c, largest;
+  a=input()
+  b=input()
+  c=input()
+  largest= compare(a,b,c)
+  output(a,b,c,largest);
+  return 0;
+}
